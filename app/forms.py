5230e29e5153a33ccc48339b2f772e401a48eb44
edigalatha4 <edigalatha4@gmail.com>
2023-04-20 17:37:55 +0530

    first commit

---
+++ app/forms.py
@@ -0,0 +1,19 @@
+from app.models import *
+from django import forms
+
+class TopicForm(forms.ModelForm):
+    class Meta():
+        model=Topic
+        fields='__all__'
+
+
+class WebpageForm(forms.ModelForm):
+    class Meta():
+        model=Webpage
+        fields='__all__'
+
+
+class AccessRecordForm(forms.ModelForm):
+    class Meta():
+        model=AccessRecord
+        fields='__all__'
